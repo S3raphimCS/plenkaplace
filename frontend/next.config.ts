5587ca559e907c429698f50d5ec3883b20b82acf
Andrew <hydra1337channel@gmail.com>
2025-04-16 00:29:47 +1000

    Change remote patterns for image queries

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from 'next';
+import dns from 'node:dns';
+
+dns.setDefaultResultOrder('ipv4first');
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_DOMAIN_NAME! ?? 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: process.env.NEXT_PUBLIC_DOMAIN_NAME ?? '127.0.0.1',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
   },
 };
 
